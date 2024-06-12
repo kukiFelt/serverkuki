b9ea8c9e9e999dbf7112dfd036b33cf3e0312021
kukiFelt <108556456+kukiFelt@users.noreply.github.com>
2024-06-12 00:03:49 +0000

    fix: delete

--- controllers/student.ts
+++ controllers/student.ts
@@ -1,25 +1,54 @@
 import { pool } from "../shared/database";
 import { Request, Response, Router } from "express";
 
+export async function deleteStudent(req: Request, res: Response) {
+  //conecta com o banco
+  const client = await pool.connect();
+  const id = req.params.id
+  try {
+    const response = await client.query('delete from students where id=${id}');
+    res.status(200).json({"message": "Registro Excluido"})
+  } catch (error) {
+    res.status(404).json({message:error})
+  }finally {
+client.release()
+  }
+}
 
 export async function listStudent(req: Request, res: Response) {
   //conecta com o banco
   const client = await pool.connect();
-  //realiza consulta sql
-  const students = await client.query(`select * from students`)
-  //retorna consulta em formato json
-  return res.status(200).json(students);
+  try {
+    //realiza consulta sql
+    const students = await client.query(`select * from students`)
+    if (students.rowCount === 0) {
+      return res.status(404).json({ message: "não encontrado" })
+    }
+    //retorna consulta em formato json
+    return res.status(200).json(students.rows);
+  }catch (error){
+    console.log(error)
+  }finally{
+    client.release;
+  }
 }
 export async function saveStudent(req: Request, res: Response) {
-  const students = req.body;
-  //verifica o erro
-  console.log(students)
   //conecta com o banco
- const client = await pool.connect();
- //realiza consulta sql
- const response = await client.query(`INSERT INTO students (name, email) VALUES ('${students.name}','${students.email}')`)
-res.status(201).json(response);
+  const client = await pool.connect();
+  const students = req.body;
+  console.log(students);
+  try {
+    //realiza consulta sql
+    const response = await client.query(`INSERT INTO students (name, email) VALUES ('${students.name}','${students.email}')`)
+    res.status(201).json(response.rows);
+    //verifica o erro
+    console.log(response.rows[0]);
+    res.status(201).json(response.rows[0]);
+  } catch (error) {
+    res.status(400).json({ message: 'Dados invalidos', error });
+  } finally {
+    client.release();
+  }
 }
-
 
 
--- router.ts
+++ router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { listCourse, saveCourse } from "./controllers/course";
-import { listStudent, saveStudent } from "./controllers/student";
+import { listStudent, saveStudent, deleteStudent } from "./controllers/student";
 const router = Router();
 
 router.get("/courses", listCourse);
@@ -8,5 +8,6 @@ router.post("/courses", saveCourse);
 
 router.get("/students", listStudent);
 router.post("/students", saveStudent);
+router.delete("/students/:id", deleteStudent);
 
 export { router };
