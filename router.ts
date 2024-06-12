b8a7a6e71d0064de9bbccb2596430330286b029b
kukiFelt <108556456+kukiFelt@users.noreply.github.com>
2024-06-12 01:48:51 +0000

    feat: Delete

--- router.ts
+++ router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { listCourse, saveCourse } from "./controllers/course";
+import { deleteCourse, listCourse, saveCourse } from "./controllers/course";
 import { listStudent, saveStudent, deleteStudent } from "./controllers/student";
 const router = Router();
 
 router.get("/courses", listCourse);
 router.post("/courses", saveCourse);
+router.delete("/courses/:id", deleteCourse);
 
 router.get("/students", listStudent);
 router.post("/students", saveStudent);
